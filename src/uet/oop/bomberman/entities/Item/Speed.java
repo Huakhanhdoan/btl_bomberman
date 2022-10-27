22c61f87c4e7be76ffc64b73cd50a88fa471f996
Huakhanhdoan <21020759@vnu.edu.vn>
2022-10-27 23:36:04 +0700

    item

---
+++ src/uet/oop/bomberman/entities/Item/Speed.java
@@ -0,0 +1,10 @@
+package uet.oop.bomberman.entities.Item;
+
+import javafx.scene.image.Image;
+
+public class Speed extends Item{
+    public Speed(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+}
